766b5c2e73be5f14dbe6cfbedb338291bd86d8de
SzymonPietraszek <SzymonPietraszek1997@gmail.com>
2026-03-19 08:19:18 +0100

    Add application/json header

--- functions/timestamps.js
+++ functions/timestamps.js
@@ -1,13 +1,6 @@
 import { createSignedFetcher } from 'aws-sigv4-fetch';
 
 export const onRequest = async (context) => {
-    console.log({
-    request: context.request,
-    url: context.request.url,
-    method: context.request.method,
-    body: context.request.body,
-    });
-
     const signedFetch = createSignedFetcher({
         service: 'lambda',
         region: 'eu-central-1',
@@ -19,21 +12,32 @@ export const onRequest = async (context) => {
 
     const incoming = new URL(context.request.url);
     const backendUrl = new URL(incoming.pathname + incoming.search, context.env.BACKEND_URL);
-    const bodyText =  context.request.method !== 'GET' ? await context.request.text() : undefined;
 
     console.log({
         backendUrl: backendUrl,
-        bodyText: bodyText,
     });
 
-    const response = await signedFetch(backendUrl, {
-        method: context.request.method,
-        body: bodyText,
-    });
+    let response;
+    if (context.request.method == 'GET') {
+        response = await signedFetch(backendUrl);
+    } else {
+        const body =  await context.request.text();
+        console.log({
+            method: 'POST',
+            body: body,
+        });
+        response = await signedFetch(backendUrl, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: body,
+        });
+    }
 
+    const responseBody = await response.text();
     console.log({
-        response: response,
         status: response.status,
+        responseBody: responseBody,
+        headers: Object.fromEntries(response.headers.entries()),
     });
 
     return response;
